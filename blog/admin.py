ea65158952e70d08fc5c164dc0ecf88b75f8daeb
embolatado <embolatado@gmail.com>
2021-05-08 12:36:49 -0500

    Vid.05 admin.py Filtros de búsqueda

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,10 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("title", "author", "date_posted")
+  readonly_fields = ('date_posted', 'last_update')
+  search_fields = ("title", "author", "date_posted")
+  date_hierarchy = "date_posted"
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=90)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    last_update = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=90, verbose_name="título")
+    content = models.TextField(verbose_name="contenido")
+    date_posted = models.DateTimeField(default=timezone.now, verbose_name="creado")
+    last_update = models.DateTimeField(auto_now=True, verbose_name="modificado")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="por")
 
     def __str__(self):
         return self.title
