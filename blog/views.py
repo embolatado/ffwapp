7bfd1ecd72eb090a7af623b84659d32539e6d55b
embolatado <embolatado@gmail.com>
2021-05-08 00:34:04 -0500

    Vid.05 28m Post from DB & Dummy data deleted

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        "titulo": "Primer post de blog",
-        "contenido": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
-        "autor": "Juan Díaz",
-        "fecha_publicado": "6 de mayo de 2021"
-    },
-    {
-        "titulo": "Segunda publicación de blog",
-        "contenido": "Eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.",
-        "autor": "María López",
-        "fecha_publicado": "8 de mayo de 2021"
-    }
-]
-
-
 def inicio(request):
-    contexto = {"publicaciones": posts}
+    contexto = {"publicaciones": Post.objects.all()}
     return render(request, 'blog/inicio.html', contexto)
 
 
