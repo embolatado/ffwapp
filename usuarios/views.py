ec4c13e1a0fc3b7a6e26c934fcffdb6d1761c167
embolatado <embolatado@gmail.com>
2021-05-09 12:42:33 -0500

    Vid.06 37:16 forms.py para campo email

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UsuarioRegistroForm
 
 # Create your views here.
 
 def registro(request):
     if request.method == "POST":
-        # CREA UN FORM CON LA INFO PASADA EN `POST`
-        formulario = UserCreationForm(request.POST)
+        # RECREA UN FORM CON LA INFO PASADA EN `POST` EN CASO ERROR
+        formulario = UsuarioRegistroForm(request.POST)
 
         if formulario.is_valid():
+            formulario.save()
             usrnme = formulario.cleaned_data.get('username')
-            # FLASH MESSAGES
+            # FLASH MESSAGES CON f' NOTATION
             messages.success(request, f'¡Cuenta, {usrnme} creada con éxito!')
+            
             return redirect('blog-inicio')
     else:
-        formulario = UserCreationForm() # RETORNA EL FORMULARIO VACÍO
+        formulario = UsuarioRegistroForm() # RETORNA EL FORMULARIO VACÍO
     
     # CUANDO EL NO HA SIDO DETECTADO UN METHOD
     return render(request, "usuarios/registro.html", {'form_reg': formulario})
